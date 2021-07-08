0ffc7abc21f4cb23bff727ad4881ea72b2f13a0c
sun <sun@DESKTOP-GP6QFAV>
2021-07-08 18:49:20 +0100

    08/07

--- src/layouts/SignedIn.jsx
+++ src/layouts/SignedIn.jsx
@@ -6,7 +6,7 @@ export default function SignedIn({signOut}) {
     return (
         <div>
             <Menu.Item>
-                <Image avatar spaced="right" src="C:\Users\sun\Desktop\avatar.jpeg" />
+                <Image avatar spaced="right" src="https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_960_720.png" />
                 <Dropdown pointing="top left" text="Gunes">
                     <Dropdown.Menu>
                         <Dropdown.Item text="Bilgilerim" icon="info" />
---
+++ src/pages/ProductAdd.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+import { FormField, Button, Label } from 'semantic-ui-react'
+import KodlamaIoTextInput from '../utilities/customFormControls/KodlamaIoTextInput'
+
+export default function ProductAdd() {
+    const initialValues = { productName: "", unitPrice: 10 }
+
+    const schema = Yup.object({
+        productName: Yup.string().required("Urun adi zorunlu"),
+        unitPrice: Yup.number().required("Urun fiyati zorunlu")
+    })
+
+    return (
+
+        <Formik
+            initialValues={initialValues}
+            validationSchema={schema}
+            onSubmit={(values) => {
+                console.log(values)
+            }}
+        >
+            <Form className="ui form">
+                <KodlamaIoTextInput name="productName" placeholder="Urun Adi" />
+                <KodlamaIoTextInput name="unitPrice" placeholder="Urun Fiyati" />
+                {/* <FormField>
+                    <Field name="productName" placeholder="Urun Adi"></Field>
+                    <ErrorMessage name="productName" render={error =>
+                        <Label pointing basic color="red" content={error}></Label>
+                    }></ErrorMessage>
+                </FormField> */}
+                <Button color="green" typr="submit">Ekle</Button>
+            </Form>
+        </Formik>
+
+    )
+}
